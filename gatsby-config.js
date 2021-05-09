06c8cfb0a4659dd45c9785dc3d1be7568e3f7104
eugeneyan <eugeneyanziyou@gmail.com>
2021-05-09 16:14:51 -0700

    Update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,6 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
-module.exports = {
-  /* Your site config here */
-  plugins: [],
+ module.exports = {
+  pathPrefix: "/applied-ml-site",
 }
