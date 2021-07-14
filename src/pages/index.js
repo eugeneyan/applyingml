2656b6d8fdae87568b5e17f560519487f52f59a2
eugeneyan <eugeneyanziyou@gmail.com>
2021-07-13 19:59:55 -0700

    Add eslintrc

--- src/pages/index.js
+++ src/pages/index.js
@@ -46,9 +46,9 @@ export default function Home() {
         <Link to="/papers/">curated papers/blogs</Link>,{" "}
         <Link to="/resources/">guides</Link>, and{" "}
         <Link to="/mentors/">interviews</Link> with ML practitioners. In a
-        nutshell, we're{" "}
+        nutshell, it's{" "}
         <Link to="/about/">
-          1/3 applied-ml, 1/3 ghost knowledge, 1/3 Tim Ferriss Show
+          1/3 applied-ml, 1/3 ghost knowledge, and 1/3 Tim Ferriss Show
         </Link>
         . The intent is to make it easier to apply—and benefit from—ML at work.
       </p>
